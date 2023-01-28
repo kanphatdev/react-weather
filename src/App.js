b832346acbbd196398e2ea80c75a30f590fcd4c7
kanphatratrazer <kanphatratrazer28@gmail.com>
2023-01-28 09:38:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './defult.css'
+import keys from './keys';
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+const api = {
+  key: keys.API_KEY,
+  base: keys.BASE_URL
+}
 function App() {
+  const databuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return date;
+  }
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+        .then(res => res.json())
+        .then((results) => {
+          setQuery("");
+          setWeather(results);
+          console.log(results);
+        })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={typeof weather.main != "undefined"? weather.main.temp > 30 ? "app hot" :" app cold" :"app"}>
+      <div className="search-container my-4">
+        <div className='row'>
+          <div className='col-9 my-2'>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="search"
+              multiline
+              maxRows={4}
+              fullWidth
+              className='mx-5'
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+        </div>
+      </div>
+      {typeof weather.main != "undefined" ? (
+        <div>
+          <div className="location-container">
+            <div className="location">
+              {weather.name}, {weather.sys.country}
+            </div>
+            <div className='date'>{databuild(new Date())}</div>
+            <div className='weather-container'>
+              <div className='temperature'>
+                {Math.round( weather.main.temp)}°C
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        </div>
+      ) : (""
+
+      )}
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+module.exports ={
+    API_KEY:"4bf81f897d8d68a0e754e8451c13660e",
+    BASE_URL:"https://api.openweathermap.org/data/2.5/"
+}
